e1593eb3c94eecf1aefe4f43a84bb181493c876e
Unknown <ccatlett2000@mctherealm.net>
2017-12-14 20:19:48 -0500

    Add temp website

---
+++ config.js
@@ -0,0 +1,17 @@
+var api = "https://pool.fearsomegaming.com:8117";
+
+var coinUnits = 100000000;
+
+var poolHost = "pool.fearsomegaming.com";
+
+var irc = "irc.freenode.net/#bcn-pools";
+
+var email = "ccatlett2000@mctherealm.net";
+
+var cryptonatorWidget = ["BCN-BCH", "BCN-USD"];
+
+var easyminerDownload = "https://github.com/zone117x/cryptonote-easy-miner/releases/";
+
+var blockchainExplorer = "https://chainradar.com/bcn/block/";
+
+var transactionExplorer = "https://chainradar.com/bcn/transaction/";
